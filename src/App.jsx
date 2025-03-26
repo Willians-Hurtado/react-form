987d5580927e02c7373e3a9afcfe52b18b620492
Willians-Hurtado <willianshurtado5@gmail.com>
2025-03-26 21:33:35 +0100

    add milestone 1

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,26 @@
 import { useState } from 'react'
-import Main from './components/Main'
+
+
+const articles = [
+  { id: 1, title: "React", topics: "Come imparare React" },
+  { id: 2, title: "Angular", topics: "Come imparare Angular" },
+  { id: 3, title: "Vue", topics: "Come imparare Vue" }
+
+]
 
 function App() {
 
 
   return (
     <>
-      <Main></Main>
+      <div>
+        {articles.map((article) => (
+          <div key={article.id}>{article.title}</div>
+        ))}
+
+
+      </div>
+
     </>
   )
 }
